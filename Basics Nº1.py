45ec79601e768c36ada086f5eb2450f252362ef8
FSP-1 <f.s.p282002@gmail.com>
2023-08-17 17:20:08 +0200

    Commit desde El Psy

--- Basics Nº1.py
+++ Basics Nº1.py
@@ -1,9 +1,10 @@
-Age= 23
-print("Hey world im starting to learn how to used phyton and also to write in my keyboard whitout "+str(Age)+" looking it")
-print("hello",Age,"agent")
-#Este modo es el mejor
+Age = 23
+print("Hey world im starting to learn how to used python "+str(Age)+" looking it")
+print("hello", Age, "agent")
+# Este modo es el mejor
 print(f"You are {Age} old you DOG")
-x, y ,z= 0,2,3
-print(z,x)
-x= y =z= 2023
-print(z,x,y)
+x, y, z = 0, 2, 3
+print(z, x)
+x = y2 = z = 2023
+print(z, x, y2)
+# Hey
