02e3243e82428a5e0c8576d6207478aa15599bcf
FSP-1 <f.s.p282002@gmail.com>
2023-08-17 17:15:03 +0200

    Basico

--- Basics Nº1.py
+++ Basics Nº1.py
@@ -1,1 +1,9 @@
-print("Hey world im starting to learn how to used phyton and also to write in my keyboard whitout looking it")
+Age= 23
+print("Hey world im starting to learn how to used phyton and also to write in my keyboard whitout "+str(Age)+" looking it")
+print("hello",Age,"agent")
+#Este modo es el mejor
+print(f"You are {Age} old you DOG")
+x, y ,z= 0,2,3
+print(z,x)
+x= y =z= 2023
+print(z,x,y)
